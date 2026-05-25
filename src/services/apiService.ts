48a66ed49238a5f13aa04d754ac39d0d0fe01963
Jan Ondrovčák <ondrovcakjan@gmail.com>
2026-05-25 14:01:33 +0200

    fix: vzhled

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,31 +1,31 @@
 const API_BASE_URL = "https://fakestoreapi.com";
 
- async function apiFetch<T>(endpoint: string, options?: RequestInit) : Promise<T> {
-const response = await fetch(`${API_BASE_URL}${endpoint}`, options);
- 
-if(!response.ok) {
-    throw new Error(`API request failed with status ${response.status}`);
-}
+async function apiFetch<T>(
+  endpoint: string,
+  options?: RequestInit,
+): Promise<T> {
+  const response = await fetch(`${API_BASE_URL}${endpoint}`, options);
 
-return response.json();
- }
+  if (!response.ok) {
+    throw new Error(`API request failed with status ${response.status}`);
+  }
+
+  return response.json();
+}
 
 export type Product = {
-    id: number;
-    title: string;
-    price: number;
-    description: string;
-    category: string;
-    image: string;
-    rating: {
-        rate: number;
-        count: number;
-    };
-}
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};
 
 export const getProducts = () => apiFetch<Product[]>("/products");
 
 export const getProduct = (id: number) => apiFetch<Product>(`/products/${id}`);
-
-
-
