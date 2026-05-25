b873b0176891caa1e85eebbecbc13329e383048e
filip <gemela.filip@gmail.com>
2026-05-25 13:59:05 +0200

    Changed fetch to axios

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,31 +1,28 @@
+import axios, { type AxiosRequestConfig } from "axios";
+
 const API_BASE_URL = "https://fakestoreapi.com";
 
- async function apiFetch<T>(endpoint: string, options?: RequestInit) : Promise<T> {
-const response = await fetch(`${API_BASE_URL}${endpoint}`, options);
- 
-if(!response.ok) {
-    throw new Error(`API request failed with status ${response.status}`);
+async function apiFetch<T>(
+  endpoint: string,
+  options?: AxiosRequestConfig,
+): Promise<T> {
+    const response = await axios(`${API_BASE_URL}${endpoint}`, options);
+    return response.data;
 }
-
-return response.json();
- }
 
 export type Product = {
-    id: number;
-    title: string;
-    price: number;
-    description: string;
-    category: string;
-    image: string;
-    rating: {
-        rate: number;
-        count: number;
-    };
-}
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};
 
 export const getProducts = () => apiFetch<Product[]>("/products");
 
 export const getProduct = (id: number) => apiFetch<Product>(`/products/${id}`);
-
-
-
