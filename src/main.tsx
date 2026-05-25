3654c8bbd171b5cd726a589255f50c56e0f1fb22
filip <gemela.filip@gmail.com>
2026-05-25 09:41:33 +0200

    File tree layout change

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import router from "./routes/routes";
 import "./index.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import Header from "../components/common/Header";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Header />
+      <h1>Homepage</h1>
+    </div>
+  );
+}
