037053ca522104ddfddacf74e36761f14ea548ef
filip <gemela.filip@gmail.com>
2026-05-27 10:47:01 +0200

    Added card context to ProductDetail

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -10,6 +10,7 @@ import { AxiosError } from "axios";
 import QuantityCounter from "../components/common/QuantityCounter";
 import { Rating } from "react-simple-star-rating";
 import RelatedList from "../components/features/RelatedList";
+import Cart from "../components/common/Cart";
 
 const ProductDetail = () => {
   const { id } = useParams();
@@ -50,96 +51,98 @@ const ProductDetail = () => {
 
   const description = `${product.description.charAt(0).toUpperCase()}${product.description.slice(1)}`;
   return (
-    <div>
-      <Header />
-      <div className="max-w-6xl mx-auto px-6 py-8 mt-15 ml-8">
-        {/* breadcrumb */}
-        <div className="mb-6 flex items-center gap-2 text-sm text-gray-600 ml-8">
-          <Link to="/" className="hover:text-black">
-            Home
-          </Link>
-          <span>-</span>
-          <Link
-            to={`/category/${product.category}`}
-            className="capitalize hover:text-black"
-          >
-            {product.category}
-          </Link>
-          <span>-</span>
-          <span className="text-black">{product.title}</span>
+    <Cart>
+      <div>
+        <Header />
+        <div className="max-w-6xl mx-auto px-6 py-8 mt-15 ml-8">
+          {/* breadcrumb */}
+          <div className="mb-6 flex items-center gap-2 text-sm text-gray-600 ml-8">
+            <Link to="/" className="hover:text-black">
+              Home
+            </Link>
+            <span>-</span>
+            <Link
+              to={`/category/${product.category}`}
+              className="capitalize hover:text-black"
+            >
+              {product.category}
+            </Link>
+            <span>-</span>
+            <span className="text-black">{product.title}</span>
+          </div>
+
+          {/* main grid */}
+          <div className="grid grid-cols-[55%_45%]  gap-10">
+            {/* levý sloupec */}
+
+            <div className="relative rounded-2xl bg-gray-100 p-10 flex items-center justify-center overflow-hidden ml-8 w-105">
+              <img
+                src={product.image}
+                alt={product.title}
+                onClick={() => setLightboxOpen(true)}
+                className="transition duration-300 hover:scale-110 cursor-pointer"
+              />
+            </div>
+
+            {/* pravý sloupec */}
+            <div className="flex flex-col gap-4 w-full">
+              <span className="text-xs font-semibold uppercase tracking-widest text-primary bg-[#e4e4fa] px-3 py-1 rounded-full w-fit">
+                {product.category}
+              </span>
+              <h1 className="text-4xl font-bold capitalize">{product.title}</h1>
+              <div className="flex items-center gap-2">
+                <Rating
+                  initialValue={product.rating.rate}
+                  SVGstyle={{ display: "inline" }}
+                  readonly
+                  allowFraction
+                  size={20}
+                />
+                <span className="text-sm text-gray-600">
+                  {product.rating.count} Reviews
+                </span>
+              </div>
+              <p className="text-3xl font-bold">${product.price}</p>
+              <p className="text-sm text-gray-500">
+                Free shipping on orders over $1'000'000.
+              </p>
+
+              <p className="text-gray-700 mt-6 max-w-2xl ">{description}</p>
+              <div className="flex flex-col gap-3 mt-4">
+                <div className="flex items-center gap-3">
+                  {/* quantity counter */}
+                  <QuantityCounter
+                    quantity={quantity}
+                    onIncrease={() => setQuantity((q) => q + 1)}
+                    onDecrease={() => setQuantity((q) => Math.max(1, q - 1))}
+                  />
+                  {/* add to cart button */}
+                  <button className="flex-1 bg-green-800 hover:bg-green-900 text-white font-semibold py-2 rounded-lg transition duration-200 text-bg cursor-pointer">
+                    Add to Cart
+                  </button>
+                </div>
+                {/* info card pod add to cart btn */}
+              </div>
+            </div>
+          </div>
         </div>
+        <hr className="border-t border-gray-100 mt-19" />
+        <RelatedList products={related} />
 
-        {/* main grid */}
-        <div className="grid grid-cols-[55%_45%]  gap-10">
-          {/* levý sloupec */}
-
-          <div className="relative rounded-2xl bg-gray-100 p-10 flex items-center justify-center overflow-hidden ml-8 w-105">
+        {lightboxOpen && (
+          <div
+            className="fixed inset-0 bg-black/80 flex items-center justify-center z-50"
+            onClick={() => setLightboxOpen(false)}
+          >
             <img
               src={product.image}
               alt={product.title}
-              onClick={() => setLightboxOpen(true)}
-              className="transition duration-300 hover:scale-110 cursor-pointer"
+              className="max-h-100 max-w-100 object-contain"
             />
           </div>
-
-          {/* pravý sloupec */}
-          <div className="flex flex-col gap-4 w-full">
-            <span className="text-xs font-semibold uppercase tracking-widest text-primary bg-[#e4e4fa] px-3 py-1 rounded-full w-fit">
-              {product.category}
-            </span>
-            <h1 className="text-4xl font-bold capitalize">{product.title}</h1>
-            <div className="flex items-center gap-2">
-              <Rating
-                initialValue={product.rating.rate}
-                SVGstyle={{ display: "inline" }}
-                readonly
-                allowFraction
-                size={20}
-              />
-              <span className="text-sm text-gray-600">
-                {product.rating.count} Reviews
-              </span>
-            </div>
-            <p className="text-3xl font-bold">${product.price}</p>
-            <p className="text-sm text-gray-500">
-              Free shipping on orders over $1'000'000.
-            </p>
-
-            <p className="text-gray-700 mt-6 max-w-2xl ">{description}</p>
-            <div className="flex flex-col gap-3 mt-4">
-              <div className="flex items-center gap-3">
-                {/* quantity counter */}
-                <QuantityCounter
-                  quantity={quantity}
-                  onIncrease={() => setQuantity((q) => q + 1)}
-                  onDecrease={() => setQuantity((q) => Math.max(1, q - 1))}
-                />
-                {/* add to cart button */}
-                <button className="flex-1 bg-green-800 hover:bg-green-900 text-white font-semibold py-2 rounded-lg transition duration-200 text-bg cursor-pointer">
-                  Add to Cart
-                </button>
-              </div>
-              {/* info card pod add to cart btn */}
-            </div>
-          </div>
-        </div>
+        )}
       </div>
-      <hr className="border-t border-gray-100 mt-19" />
-      <RelatedList products={related} />
-
-      {lightboxOpen && (
-        <div
-          className="fixed inset-0 bg-black/80 flex items-center justify-center z-50"
-          onClick={() => setLightboxOpen(false)}
-        >
-          <img
-            src={product.image}
-            alt={product.title}
-            className="max-h-100 max-w-100 object-contain"
-          />
-        </div>
-      )}
-    </div>
+    </Cart>
   );
 };
 
