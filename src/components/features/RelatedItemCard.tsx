37859a8641fc2a3f7f27e4f4241ec74f64397305
Jan Ondrovčák <ondrovcakjan@gmail.com>
2026-05-27 09:29:29 +0200

    add: related item card komponenta

--- src/components/features/RelatedItemCard.tsx
+++ src/components/features/RelatedItemCard.tsx
@@ -1,5 +1,32 @@
-const RelatedItemCard = () => {
-  return <div>RelatedItemCard</div>;
+import { useNavigate } from "react-router";
+import type { Product } from "../../services/apiService";
+
+type Props = {
+  product: Product;
+};
+
+const RelatedItemCard = ({ product }: Props) => {
+  const navigate = useNavigate();
+  return (
+    <div
+      onClick={() => navigate(`/product/${product.id}`)}
+      className="cursor-pointer rounded-xl bg-gray-100 p-6 flex flex-col items-center gap-3 
+    hover:shadow-md transition"
+    >
+      <img
+        src={product.image}
+        alt={product.title}
+        className="h-40 object-contain"
+      />
+      <p className="text-xs text-gray-500 uppercase tracking-widest">
+        {product.category}
+      </p>
+      <p className="text-sm font-semibold text-center capitalize">
+        {product.title}
+      </p>
+      <p className="text-sm text-gray-600">${product.price}</p>
+    </div>
+  );
 };
 
 export default RelatedItemCard;
