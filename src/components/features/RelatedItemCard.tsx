7e232c7c045cecb69558dbeb822795ae214d23dd
Jan Ondrovčák <ondrovcakjan@gmail.com>
2026-05-27 10:04:47 +0200

    fix: css v related list

--- src/components/features/RelatedItemCard.tsx
+++ src/components/features/RelatedItemCard.tsx
@@ -10,21 +10,21 @@ const RelatedItemCard = ({ product }: Props) => {
   return (
     <div
       onClick={() => navigate(`/product/${product.id}`)}
-      className="cursor-pointer rounded-xl bg-gray-100 p-6 flex flex-col items-center gap-3 
-    hover:shadow-md transition"
+      className="cursor-pointer rounded-xl bg-gray-50 p-6 flex flex-col items-center gap-2 
+    hover:shadow-md transition duration-300 hover:scale-103 px-4"
     >
       <img
         src={product.image}
         alt={product.title}
         className="h-40 object-contain"
       />
-      <p className="text-xs text-gray-500 uppercase tracking-widest">
+      <p className="text-xs text-gray-700 uppercase tracking-widest">
         {product.category}
       </p>
-      <p className="text-sm font-semibold text-center capitalize">
+      <p className="text-sm font-bold text-center capitalize">
         {product.title}
       </p>
-      <p className="text-sm text-gray-600">${product.price}</p>
+      <p className="text-sm font-semibold text-black">${product.price}</p>
     </div>
   );
 };
--- src/components/features/RelatedList.tsx
+++ src/components/features/RelatedList.tsx
@@ -9,9 +9,9 @@ const RelatedList = ({ products }: Props) => {
   if (products.length === 0) return null;
 
   return (
-    <div className="mt-16">
+    <div className="mt-40 mb-15 px-6 md:px-12 lg:px-20">
       <h2 className="text-2xl font-bold mb-6">You May Also Like</h2>
-      <div className="grid grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {products.map((product) => (
           <RelatedItemCard key={product.id} product={product} />
         ))}
