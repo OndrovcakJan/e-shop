75d3a7608637fab92f0129e6ac289546e42e4593
Jan Ondrovčák <ondrovcakjan@gmail.com>
2026-05-28 11:06:39 +0200

    add: search engine

--- src/components/features/SearchBar.tsx
+++ src/components/features/SearchBar.tsx
@@ -1,12 +1,24 @@
 import { Search } from "lucide-react";
 
-export default function SearchBar() {
+type Props = {
+  value: string;
+  onChange: (value: string) => void;
+};
+
+export default function SearchBar({ value, onChange }: Props) {
   return (
     <form className="flex bg-[#e6eaf6] border border-gray-400 rounded-2xl pl-0.5 items-center">
       <label htmlFor="search">
-        <Search className="h-4"/>
+        <Search className="h-4" />
       </label>
-      <input type="text" id="search" placeholder="Search for products" className="border-none outline-none" />
+      <input
+        type="text"
+        id="search"
+        placeholder="Search for products"
+        className="border-none outline-none"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
     </form>
   );
 }
