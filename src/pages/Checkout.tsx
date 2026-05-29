c9a25cac421181d0412741cc5eda5577f84d807f
Jan Ondrovčák <ondrovcakjan@gmail.com>
2026-05-29 10:13:54 +0200

    Update Checkout.tsx

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -15,7 +15,36 @@ const Checkout = () => {
             <div className="flex flex-col gap-6">levy</div>
 
             {/* pravý sloupec */}
-            <div className="sticky top-24">pravy</div>
+
+            <div className="sticky top-24 border border-gray-200 rounded-xl p-6 flex flex-col gap-4">
+              <h2 className="font-bold text-lg">Order Summary</h2>
+              <div className="flex flex-col gap-4">
+                {cartItems.map((item) => (
+                  <div key={item.id} className="flex items-center gap-3">
+                    <img
+                      src={item.image}
+                      alt={item.title}
+                      className="w-14 h-14 object-contain rounded-lg bg-gray-100 p-1"
+                    />
+                    <div className="flex-1">
+                      <p className="text-sm font-medium capitalize">
+                        {item.title}
+                      </p>
+                      <p className="text-xs text-gray-500">
+                        Count: {item.amount}
+                      </p>
+                    </div>
+                    <span className="text-sm font-medium">
+                      {" "}
+                      ${(item.price * item.amount).toFixed(2)}
+                    </span>
+                  </div>
+                ))}
+              </div>
+
+              <hr className="border-gray-200" />
+            </div>
+
           </div>
         </div>
         <Footer />
